e428a9a5d457a106b8d59b26e5f7d922e078c2d1
Aman Braich <braich_aman3@yahoo.ca>
2024-04-22 15:07:54 -0400

    Added Controller class for camera transistions, angles sounds...

--- src/Controller.js
+++ src/Controller.js
@@ -8,12 +8,193 @@ export default class Controller {
         this.sounds = this.pwa.sounds;
 
         this.playWhooshBool = true
+        this.isTopPortal = false
+
+        this.isTopPortal = false
+        this.isTopBase = false
+        this.isMineCave = false
+        this.isGreenHouse = false
+        this.isFree = true
+
+        const btns = document.querySelector('.btnGroup');
+        const buttonHideTime = 6000
 
         this.setCamControls();
         this.setInteractiveControls();
+        this.setButtonControls();
+
+        document.getElementById("Top").onclick = function () {
+            btns.style.display = 'none';
+            this.pwa = new PWA();
+            this.controller = this.pwa.controller
+            this.myCamera = this.pwa.myCamera
+
+
+            this.isTopBase = this.controller.isTopBase
+            this.isTopPortal = this.controller.isTopPortal
+            this.isMineCave = this.controller.isMineCave
+            this.isGreenHouse = this.controller.isGreenHouse
+            this.isFree = this.controller.isFree
+
+            this.sounds = this.pwa.sounds
+            this.sounds.playClick()
+            if (this.isTopBase == true) {
+
+            } else if (this.isTopPortal == true) {
+                this.myCamera.camAngles.homeDock()
+                this.myCamera.transitions.negX();
+                setTimeout(() => {
+                    this.controller.buttonControls.topBase()
+                }, 2000);
+            } else if (this.isMineCave == true) {
+                this.myCamera.camAngles.homeDock()
+                this.myCamera.transitions.posX();
+                setTimeout(() => {
+                    this.controller.buttonControls.topBase()
+                }, 2000);
+            } else if (this.isGreenHouse == true) {
+                this.myCamera.camAngles.homeDock()
+                this.myCamera.transitions.negZ();
+                setTimeout(() => {
+                    this.controller.buttonControls.topBase()
+                }, 2000);
+            } else if (this.isFree == true) {
+                this.controller.buttonControls.topBase()
+            }
+            setTimeout(() => {
+                btns.style.display = 'inline-flex';
+            }, buttonHideTime);
+        }
+
+        document.getElementById("Abyss").onclick = function () {
+            btns.style.display = 'none';
+            this.pwa = new PWA();
+            this.controller = this.pwa.controller
+            this.myCamera = this.pwa.myCamera
+
+            this.isTopBase = this.controller.isTopBase
+            this.isTopPortal = this.controller.isTopPortal
+            this.isMineCave = this.controller.isMineCave
+            this.isGreenHouse = this.controller.isGreenHouse
+            this.isFree = this.controller.isFree
+
+            this.sounds = this.pwa.sounds
+            this.sounds.playClick()
+            if (this.isTopBase == true) {
+                this.myCamera.camAngles.homeDock()
+                this.myCamera.transitions.posZ();
+                setTimeout(() => {
+                    this.controller.buttonControls.topPortal()
+                }, 2000);
+            } else if (this.isTopPortal == true) {
+
+            } else if (this.isMineCave == true) {
+                this.myCamera.camAngles.homeDock()
+                this.myCamera.transitions.posX();
+                setTimeout(() => {
+                    this.controller.buttonControls.topPortal()
+                }, 2000);
+            } else if (this.isGreenHouse == true) {
+                this.myCamera.camAngles.homeDock()
+                this.myCamera.transitions.negZ();
+                setTimeout(() => {
+                    this.controller.buttonControls.topPortal()
+                }, 2000);
+            } else if (this.isFree == true) {
+                this.controller.buttonControls.topPortal()
+            }
+            setTimeout(() => {
+                btns.style.display = 'inline-flex';
+            }, buttonHideTime);
+        }
+
+        document.getElementById("Collectables").onclick = function () {
+            btns.style.display = 'none';
+            this.pwa = new PWA();
+            this.controller = this.pwa.controller
+            this.myCamera = this.pwa.myCamera
+
+            this.isTopBase = this.controller.isTopBase
+            this.isTopPortal = this.controller.isTopPortal
+            this.isMineCave = this.controller.isMineCave
+            this.isGreenHouse = this.controller.isGreenHouse
+            this.isFree = this.controller.isFree
+
+            this.sounds = this.pwa.sounds
+            this.sounds.playClick()
+            if (this.isTopBase == true) {
+                this.myCamera.camAngles.homeDock()
+                this.myCamera.transitions.posZ();
+                setTimeout(() => {
+                    this.controller.buttonControls.mineCave();
+                }, 2000);
+            } else if (this.isTopPortal == true) {
+                this.myCamera.camAngles.homeDock()
+                this.myCamera.transitions.negX();
+                setTimeout(() => {
+                    this.controller.buttonControls.mineCave();
+                }, 2000);
+            } else if (this.isMineCave == true) {
+
+            } else if (this.isGreenHouse == true) {
+                this.myCamera.camAngles.homeDock()
+                this.myCamera.transitions.negZ();
+                setTimeout(() => {
+                    this.controller.buttonControls.mineCave();
+                }, 2000);
+
+            } else if (this.isFree == true) {
+                this.controller.buttonControls.mineCave();
+            }
+            setTimeout(() => {
+                btns.style.display = 'inline-flex';
+            }, buttonHideTime);
+        }
+
+        document.getElementById("Coming").onclick = function () {
+            btns.style.display = 'none';
+            this.pwa = new PWA();
+
+            this.controller = this.pwa.controller
+            this.myCamera = this.pwa.myCamera
+
+            this.isTopBase = this.controller.isTopBase
+            this.isTopPortal = this.controller.isTopPortal
+            this.isMineCave = this.controller.isMineCave
+            this.isGreenHouse = this.controller.isGreenHouse
+            this.isFree = this.controller.isFree
+
+            this.sounds = this.pwa.sounds
+            this.sounds.playClick()
+            if (this.isTopBase == true) {
+                this.myCamera.camAngles.homeDock()
+                this.myCamera.transitions.posZ();
+                setTimeout(() => {
+                    this.controller.buttonControls.greenHouse();
+                }, 2000);
+            } else if (this.isTopPortal == true) {
+                this.myCamera.camAngles.homeDock()
+                this.myCamera.transitions.negX();
+                setTimeout(() => {
+                    this.controller.buttonControls.greenHouse();
+                }, 2000);
+            } else if (this.isMineCave == true) {
+                this.myCamera.camAngles.homeDock()
+                this.myCamera.transitions.posX();
+                setTimeout(() => {
+                    this.controller.buttonControls.greenHouse();
+                }, 2000);
+            } else if (this.isGreenHouse == true) {
+
+            } else if (this.isFree == true) {
+                this.controller.buttonControls.greenHouse();
+            }
+            setTimeout(() => {
+                btns.style.display = 'inline-flex';
+            }, buttonHideTime);
+
+        }
     }
-
-
 
     setCamControls() {
         this.camControls = {}
@@ -30,6 +211,12 @@ export default class Controller {
                 }, 2000);
             }, 500);
             this.playWhooshBool = true
+            //Locations tracker
+            this.isTopPortal = false
+            this.isTopBase = false
+            this.isMineCave = false
+            this.isGreenHouse = false
+            this.isFree = true
         }
 
         this.camControls.back3 = () => {
@@ -37,13 +224,19 @@ export default class Controller {
             setTimeout(() => {
                 this.sounds.playWhoosh();
                 this.myCamera.disbleControls();
-                this.myCamera.transitions.default();
+                this.myCamera.transitions.negZ();
                 setTimeout(() => {
                     this.myCamera.camAngles.homeDock();
                     this.myCamera.enableControls();
                 }, 2000);
             }, 500);
             this.playWhooshBool = true
+            //Locations tracker
+            this.isTopPortal = false
+            this.isTopBase = false
+            this.isMineCave = false
+            this.isGreenHouse = false
+            this.isFree = true
         }
 
         this.camControls.topBase = () => {
@@ -60,6 +253,12 @@ export default class Controller {
                     this.myCamera.enableControls();
                 }, 2000);
             }, 500);
+            //Locations tracker
+            this.isTopPortal = false
+            this.isTopBase = true
+            this.isMineCave = false
+            this.isGreenHouse = false
+            this.isFree = false
         }
 
         this.camControls.mineCave = () => {
@@ -76,6 +275,12 @@ export default class Controller {
                     this.myCamera.enableControls();
                 }, 2000);
             }, 500);
+            //Locations tracker
+            this.isTopPortal = false
+            this.isTopBase = false
+            this.isMineCave = true
+            this.isGreenHouse = false
+            this.isFree = false
         }
 
         this.camControls.greenHouse = () => {
@@ -92,6 +297,12 @@ export default class Controller {
                     this.myCamera.enableControls();
                 }, 2000);
             }, 500);
+            //Locations tracker
+            this.isTopPortal = false
+            this.isTopBase = false
+            this.isMineCave = false
+            this.isGreenHouse = true
+            this.isFree = false
         }
 
         this.camControls.topPort = () => {
@@ -108,6 +319,12 @@ export default class Controller {
                     this.myCamera.camAngles.topPortalAngle();
                 }, 2000);
             }, 500);
+            //Locations tracker
+            this.isTopPortal = true
+            this.isTopBase = false
+            this.isMineCave = false
+            this.isGreenHouse = false
+            this.isFree = false
         }
 
         this.camControls.greenSign = () => {
@@ -124,6 +341,12 @@ export default class Controller {
                     this.myCamera.enableControls();
                 }, 2000);
             }, 500);
+            //Locations tracker
+            this.isTopPortal = false
+            this.isTopBase = false
+            this.isMineCave = false
+            this.isGreenHouse = true
+            this.isFree = false
         }
     }
 
@@ -141,4 +364,83 @@ export default class Controller {
         }
     }
 
+    setButtonControls() {
+        this.buttonControls = {}
+
+        this.buttonControls.topBase = () => {
+            setTimeout(() => {
+                this.sounds.playWhoosh();
+                this.playWhooshBool = false
+                this.myCamera.disbleControls();
+                this.myCamera.transitions.topBaseTransition()
+                setTimeout(() => {
+                    this.myCamera.camAngles.topBaseAngle()
+                    this.myCamera.enableControls();
+                }, 2000);
+            }, 500);
+            //Locations tracker
+            this.isTopPortal = false
+            this.isTopBase = true
+            this.isMineCave = false
+            this.isGreenHouse = false
+            this.isFree = false
+        }
+
+        this.buttonControls.mineCave = () => {
+            setTimeout(() => {
+                this.sounds.playWhoosh();
+                this.playWhooshBool = false
+                this.myCamera.disbleControls();
+                this.myCamera.transitions.caveMineArcTransistion();
+                setTimeout(() => {
+                    this.myCamera.camAngles.mineAngle();
+                    this.myCamera.enableControls();
+                }, 3000);
+            }, 500);
+            //Locations tracker
+            this.isTopPortal = false
+            this.isTopBase = false
+            this.isMineCave = true
+            this.isGreenHouse = false
+            this.isFree = false
+        }
+
+        this.buttonControls.topPortal = () => {
+            setTimeout(() => {
+                this.sounds.playWhoosh();
+                this.playWhooshBool = false
+                this.myCamera.disbleControls();
+                this.myCamera.transitions.topPortalArcTransistion();
+                setTimeout(() => {
+                    this.myCamera.enableControls();
+                    this.myCamera.camAngles.topPortalAngle();
+                }, 2000);
+            }, 500);
+            //Locations tracker
+            this.isTopPortal = true
+            this.isTopBase = false
+            this.isMineCave = false
+            this.isGreenHouse = false
+            this.isFree = false
+        }
+
+        this.buttonControls.greenHouse = () => {
+            setTimeout(() => {
+                this.sounds.playWhoosh();
+                this.playWhooshBool = false
+                this.myCamera.disbleControls();
+                this.myCamera.transitions.greenHouseArcTransition();
+                setTimeout(() => {
+                    this.myCamera.camAngles.greenHouseAngle();
+                    this.myCamera.enableControls();
+                }, 2000);
+            }, 500);
+            //Locations tracker
+            this.isTopPortal = false
+            this.isTopBase = false
+            this.isMineCave = false
+            this.isGreenHouse = true
+            this.isFree = false
+        }
+    }
 }
