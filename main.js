17cedb56ccecafeabc321ce97985edd09297d238
Aman Braich <braich_aman3@yahoo.ca>
2024-05-29 16:54:59 -0400

    Mobile view control

--- main.js
+++ main.js
@@ -2,4 +2,26 @@ import './style.css'
 
 import PWA from './src/PWA'
 
-const pwa = new PWA(document.querySelector(".webgl"))
+
+
+let details = navigator.userAgent;
+
+let regexp = /android|iphone|kindle|ipad/i;
+
+let isMobileDevice = regexp.test(details);
+
+const btnGp = document.getElementById('btnGp');
+const loading = document.getElementById('loading')
+const canvas = document.getElementById('canvas')
+const img = document.getElementById('imgBox')
+
+if (isMobileDevice) {
+    btnGp.style.display = 'none'
+    loading.style.display = 'none'
+    canvas.style.display = 'none'
+} else {
+    
+    const pwa = new PWA(document.querySelector(".webgl"))
+    img.style.display = 'none'
+}
+
