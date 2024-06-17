714536ee18d2e6144d03bf4233857d56bb2dd87e
Aman Braich <braich_aman3@yahoo.ca>
2024-06-17 16:03:50 -0400

    hide socials in mobile view

--- main.js
+++ main.js
@@ -14,11 +14,13 @@ const btnGp = document.getElementById('btnGp');
 const loading = document.getElementById('loading')
 const canvas = document.getElementById('canvas')
 const img = document.getElementById('imgBox')
+const linksGp = document.getElementById('linksGp')
 
 if (isMobileDevice) {
     btnGp.style.display = 'none'
     loading.style.display = 'none'
     canvas.style.display = 'none'
+    linksGp.style.display = 'none'
 } else {
     
     const pwa = new PWA(document.querySelector(".webgl"))
